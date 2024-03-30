5f31c53085ddc037dc1e9b667e05a45ea849ae22
AnawaratneMANA <it19086408@my.sliit.lk>
2024-03-30 12:07:46 +0530

    Add navigation for the user to login screen to registration screen

--- app/src/main/java/com/example/mobile_security_app/LoginActivity.java
+++ app/src/main/java/com/example/mobile_security_app/LoginActivity.java
@@ -2,14 +2,37 @@ package com.example.mobile_security_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Variables
+    private TextView registerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+        // Register UI Elements
+        setInterfaceElements();
+
+        // Listeners and Logic
+        registerText.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the registration activity.
+                Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setInterfaceElements() {
+        registerText = findViewById(R.id.registerLink);
     }
 }

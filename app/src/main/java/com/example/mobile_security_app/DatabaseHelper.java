5a88c6f5077ca471f79884ceddf2f19ba1a7e5fd
AnawaratneMANA <it19086408@my.sliit.lk>
2024-03-31 09:30:53 +0530

    Add the database handler implementations

---
+++ app/src/main/java/com/example/mobile_security_app/DatabaseHelper.java
@@ -0,0 +1,34 @@
+package com.example.mobile_security_app;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "secure_database.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Implement the database tables
+        String sqlCreateQuery = "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, salt TEXT, password TEXT)";
+        sqLiteDatabase.execSQL(sqlCreateQuery);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Upgrade the table when there's a change
+        String upgradeTableQuery = "DROP TABLE IF EXISTS user";
+        sqLiteDatabase.execSQL(upgradeTableQuery);
+    }
+}
